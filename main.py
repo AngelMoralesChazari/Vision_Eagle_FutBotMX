dfc4b5df2ff651c58e97e98f0f5f980679896730
Angel Morales <artes23graficas@gmail.com>
2026-06-11 13:41:03 -0600

    feat: implementar filtros de area y exclusion de personas en el pipeline de seguimiento

--- main.py
+++ main.py
@@ -1,59 +1,80 @@
 import cv2
+import numpy as np
 import supervision as sv
 from ultralytics import SAM
+
 
 def main():
     print("Iniciando Pipeline - Vision Eagle")
 
-    print("Cargando modelo SAM 3...")
+    # Cargar el modelo
+    print("Cargando modelo")
     model = SAM("sam_b.pt")
     video_path = "videos/video-893_singular_display.mov"
-    output_video_path = "videos/video_demo_robots.mp4"
+    output_video_path = "videos/analisis_final_robots.mp4"
 
-    # Lectura del video
+    # 1.- Lectura del video
     video_info = sv.VideoInfo.from_video_path(video_path=video_path)
     print(f"Video cargado correctamente: {video_info.width}x{video_info.height} a {video_info.fps:.2f} FPS")
 
-    # Configurar los anotadores gráficos de Supervision
-    mask_annotator = sv.MaskAnnotator()
-    label_annotator = sv.LabelAnnotator()  # Muestra el ID del robot en pantalla
-    trace_annotator = sv.TraceAnnotator()  # Dibuja la línea del camino que recorren
-    heatmap_annotator = sv.HeatMapAnnotator()  # Genera el mapa de calor dinámico
+    # 2. Configurar los anotadores gráficos de Supervision
+    mask_annotator = sv.MaskAnnotator(opacity=0.25)
+    label_annotator = sv.LabelAnnotator(text_scale=0.4, text_padding=3)
+    trace_annotator = sv.TraceAnnotator(trace_length=40, thickness=2)
+    heatmap_annotator = sv.HeatMapAnnotator(radius=20, opacity=0.4)
 
-    # Rastreador Oficial
+    # 3. Inicializar el Filtro Geográfico y Rastreador (ByteTrack)
     tracker = sv.ByteTrack()
 
-    print("Procesando cuadros del partido y aplicando capas de analítica...")
+    coordenadas_cancha = np.array([
+        [0, int(video_info.height * 0.35)],
+        [video_info.width, int(video_info.height * 0.35)],
+        [video_info.width, video_info.height],
+        [0, video_info.height]
+    ])
+    zona_cancha = sv.PolygonZone(polygon=coordenadas_cancha)
 
-    # Procesamiento de los cuadros
-    with sv.VideoSink(target_path = output_video_path, video_info = video_info) as sink:
-        for frame in sv.get_video_frames_generator(source_path = video_path):
+    print("Procesando cuadros del partido y aplicando filtros")
 
-            # Segmentación
-            results = model(frame, conf = 0.25, verbose = False)[0]
+    # 4. Bucle de procesamiento
+    with sv.VideoSink(target_path=output_video_path, video_info=video_info) as sink:
+        for frame in sv.get_video_frames_generator(source_path=video_path):
+
+            # Inferencia de IA
+            results = model(frame, conf=0.25, verbose=False)[0]
             detections = sv.Detections.from_ultralytics(results)
 
-            # Tracker para genrar IDs
+            # FILTRO 1: Exclusión de Personas
+            if detections.class_id is not None:
+                detections = detections[detections.class_id != 0]
+
+            # FILTRO 2: Dimensiones por Área
+            detections = detections[(detections.area > 2000) & (detections.area < 90000)]
+
+            # FILTRO 3: Zona de la Cancha
+            mascara_dentro_cancha = zona_cancha.trigger(detections=detections)
+            detections = detections[mascara_dentro_cancha]
+
+            # Actualizar Tracker con datos limpios
             detections = tracker.update_with_detections(detections)
 
-            # Etiquetas basadas en el ID
             if detections.tracker_id is not None:
                 labels = [f"Robot #{tracker_id}" for tracker_id in detections.tracker_id]
             else:
                 labels = []
 
-            # Renderizado de capas visuales sobre el cuadro original
-            annotated_frame = heatmap_annotator.annotate(scene = frame.copy(), detections = detections)
-            annotated_frame = mask_annotator.annotate(scene = annotated_frame, detections = detections)
-            annotated_frame = trace_annotator.annotate(scene = annotated_frame, detections = detections)
+            # Renderizado por capas de profundidad
+            annotated_frame = heatmap_annotator.annotate(scene=frame.copy(), detections=detections)
+            annotated_frame = mask_annotator.annotate(scene=annotated_frame, detections=detections)
+            annotated_frame = trace_annotator.annotate(scene=annotated_frame, detections=detections)
 
             if labels:
-                annotated_frame = label_annotator.annotate(scene = annotated_frame, detections = detections, labels = labels)
+                annotated_frame = label_annotator.annotate(scene=annotated_frame, detections=detections, labels=labels)
 
-            # Guardar el fotograma procesado
-            sink.write_frame(frame = annotated_frame)
+            sink.write_frame(frame=annotated_frame)
 
-    print(f"Video final se guardó en: {output_video_path}")
+    print("\nProceso completado con exito")
+    print(f"El video resultante se guardó en: {output_video_path}")
 
 
 if __name__ == "__main__":
